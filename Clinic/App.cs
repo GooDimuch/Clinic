e74d31dba114b215e9c9983e010668840f8db640
Dmitry Didenko <dmytro.didenko@logika.tech>
2019-07-02 22:46:41 +0300

    add res and configure project

---
+++ Clinic/App.cs
@@ -0,0 +1,7 @@
+﻿namespace Clinic {
+	public class App {
+		private static void Main(string[] args) {
+//			log("Yo");
+		}
+	}
+}
---
+++ Clinic/utils/Utils.cs
@@ -0,0 +1,7 @@
+﻿using System.Diagnostics;
+
+namespace Clinic.utils {
+	public class Utils {
+		private static void log(object message = null) { Debug.Write($"{message ?? "\n"}"); }
+	}
+}
